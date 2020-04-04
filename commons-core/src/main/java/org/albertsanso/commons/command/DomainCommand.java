7716b8c405cee307927bf33b42e8a606f07df3f6
Albert Sanso <albertsanso@gmail.com>
2020-04-04 23:44:17 +0200

    Added properties to DomainCommad

--- commons-core/src/main/java/org/albertsanso/commons/command/DomainCommand.java
+++ commons-core/src/main/java/org/albertsanso/commons/command/DomainCommand.java
@@ -1,4 +1,22 @@
 package org.albertsanso.commons.command;
 
+import java.time.ZonedDateTime;
+import java.util.Map;
+
 public abstract class DomainCommand {
+    private final ZonedDateTime occurredOn;
+    private final String uuid;
+
+    protected DomainCommand(ZonedDateTime occurredOn, String uuid) {
+        this.occurredOn = occurredOn;
+        this.uuid = uuid;
+    }
+
+    public ZonedDateTime getOccurredOn() {
+        return occurredOn;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
 }
