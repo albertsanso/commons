dc21969377b4e490114924dbf80b037f49dde973
Albert Sanso <albertsanso@gmail.com>
2020-04-04 22:12:22 +0200

    New properties added to events: occurredOn and event ID

--- commons-core/src/main/java/org/albertsanso/commons/event/DomainEvent.java
+++ commons-core/src/main/java/org/albertsanso/commons/event/DomainEvent.java
@@ -1,4 +1,9 @@
 package org.albertsanso.commons.event;
 
+import java.time.ZonedDateTime;
+
 public class DomainEvent extends Event {
+    public DomainEvent(ZonedDateTime occurredOn, String uuid) {
+        super(occurredOn, uuid);
+    }
 }
--- commons-core/src/main/java/org/albertsanso/commons/event/Event.java
+++ commons-core/src/main/java/org/albertsanso/commons/event/Event.java
@@ -1,4 +1,21 @@
 package org.albertsanso.commons.event;
 
+import java.time.ZonedDateTime;
+
 public abstract class Event {
+    private final ZonedDateTime occurredOn;
+    private final String uuid;
+
+    public Event(ZonedDateTime occurredOn, String uuid) {
+        this.occurredOn = occurredOn;
+        this.uuid = uuid;
+    }
+
+    public ZonedDateTime getOccurredOn() {
+        return occurredOn;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
 }
